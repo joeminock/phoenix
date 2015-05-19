a1b1a37f1666fc9e68e64a9760b6b918aca9e6af
Joe Minock <joe.minock@gmail.com>
2015-05-19 16:48:30 -0400

    Added Wistia Video API, Forward and Backward Buttons, removed Turbolinks from projects, tasks views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,19 @@
 class Task < ActiveRecord::Base
-  extend FriendlyId
-  friendly_id :title, use: [:slugged, :finders]
-  
-  belongs_to :project
+	extend FriendlyId
+	friendly_id :title, use: [:slugged, :finders]
 
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :video, presence: true
-  validates :tag, presence: true
-  validates :project, presence: true
+	belongs_to :project
+
+	validates :title, presence: true, length: { maximum: 50 }
+	validates :video, presence: true
+	validates :tag, presence: true
+	validates :project, presence: true
+
+	def next
+		project.tasks.where("tag > ? AND header = ?", tag, false).order(:tag).first
+	end
+
+	def prev
+		project.tasks.where("tag < ? AND header = ?", tag, false).order(:tag).last
+	end
 end
